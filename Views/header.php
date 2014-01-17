9284d0fe0a8a49b10a1fe74d20f46eb29aa22624
aziza <hegeromrane@gmail.com>
2014-01-17 12:21:35 +0100

    change path headr

--- Views/header.php
+++ Views/header.php
@@ -66,7 +66,7 @@
         <ul class="nav nav-pills">
         <?php if (Session::get('loggedIn') == false){ ?>
             <li class="active"> <a href="../index">Index</a></li>
-            <li class="active"> <a href="./Views/help/index">Help</a></li>
+            <li class="active"> <a href="../View/help">Help</a></li>
         <?php } ?>
         <?php if (Session::get('loggedIn') == true){ ?>
             <li class="active"><a href="../Dashboard">Dashboard</a></li>
