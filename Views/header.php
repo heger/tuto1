68a46f8006b4b0886eb8f632bab01becbefa33bd
heger <hegeromrane@gmail.com>
2014-01-17 15:35:20 +0100

    Update header.php

--- Views/header.php
+++ Views/header.php
@@ -1,19 +1,19 @@
 <html>
 <head>
     <title>Porte Feuille</title>
-    <link rel="stylesheet" href="../public/css/default.css">
-    <link rel="stylesheet" href="../public/css/flexslider.css">
-    <link rel="stylesheet" href="../public/css/bootstrap.css">
-    <link rel="stylesheet" href="../public/css/validationForm.css">
-    <script type="text/javascript" src="../public/js/jquery.js"></script>
-    <script type="text/javascript" src="../public/js/jquery-ui.js"></script>
-    <script type="text/javascript" src="../public/js/modernizr.custom.21718.js"></script>
-     <script type="text/javascript" src="../public/js/jqueryMigrate.js"></script>
-    <script type="text/javascript" src="../public/js/custom.js"></script>
-    <script type="text/javascript" src="../public/js/jquery_flexslider-min.js"></script>
-    <script type="text/javascript" src="../public/js/picnet.table.filter.min.js"></script>
-    <script type="text/javascript" src="../public/js/JqueryValidate.js"></script>
-    <script type="text/javascript" src="../public/js/LiveValidation.js"></script>
+    <link rel="stylesheet" href="<?php echo URL; ?>public/css/default.css">
+    <link rel="stylesheet" href="<?php echo URL; ?>public/css/flexslider.css">
+    <link rel="stylesheet" href="<?php echo URL; ?>public/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo URL; ?>public/css/validationForm.css">
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.js"></script>
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery-ui.js"></script>
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/modernizr.custom.21718.js"></script>
+     <script type="text/javascript" src="<?php echo URL; ?>public/js/jqueryMigrate.js"></script>
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/custom.js"></script>
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery_flexslider-min.js"></script>
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/picnet.table.filter.min.js"></script>
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/JqueryValidate.js"></script>
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/LiveValidation.js"></script>
     <script type="text/javascript">
         function imprimer()
         {
@@ -47,16 +47,14 @@
          <div class="flexslider">
                 <ul class="slides">
                     
-
                         <li>
-
-                            <img  src= "../public/images/Bourse1.jpg"/>
+                            <img  src= "<?php echo URL; ?>public/images/Bourse1.jpg"/>
                         </li>
 			<li>
-                             <img  src= "../public/images/bourse2.jpg"/>
+                             <img  src= "<?php echo URL; ?>public/images/bourse2.jpg"/>
                         </li>                   
 			<li>
-                            <img  src= "../public/images/bourse.jpg"/>
+                            <img  src= "<?php echo URL; ?>public/images/bourse.jpg"/>
                         </li>                   
                 </ul>
             </div>
@@ -65,23 +63,22 @@
   <div class="navbar-inner">-->
         <ul class="nav nav-pills">
         <?php if (Session::get('loggedIn') == false){ ?>
-            <li class="active"> <a href="/Controllers/index">Index</a></li>
-            <li class="active"> <a href="/Controllers/help">Help</a></li>
+            <li class="active"> <a href="<?php echo URL; ?>index">Index</a></li>
+            <li class="active"> <a href="<?php echo URL; ?>help">Help</a></li>
         <?php } ?>
         <?php if (Session::get('loggedIn') == true){ ?>
-            <li class="active"><a href="../Dashboard">Dashboard</a></li>
+            <li class="active"><a href="<?php echo URL; ?>Dashboard">Dashboard</a></li>
         <?php if (Session::get('role') == 'owner'){ ?>
-            <li class="active"><a href="../user">Users</a></li>
+            <li class="active"><a href="<?php echo URL; ?>user">Users</a></li>
         <?php } ?>
         
-            <li class="active"> <a href="../Dashboard/logout">LogOut</a></li>
+            <li class="active"> <a href="<?php echo URL; ?>Dashboard/logout">LogOut</a></li>
         <?php }else{ ?>
-            <li class="active"> <a href="/Views/login">Login</a></li>
+            <li class="active"> <a href="<?php echo URL; ?>login">Login</a></li>
         <?php } ?>
         </ul>
         </div>
 <!--  </div>
 </div>-->
     <div id="content">
-
 
