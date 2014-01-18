7eb2f6c337c16edd7a4ecf1ec66b9a8674de97dd
aziza <hegeromrane@gmail.com>
2014-01-18 07:08:49 +0100

    Vx 0.2

---
+++ Views/contact/index.php
@@ -0,0 +1,13 @@
+<center><h1>Users</h1></center>
+<div class="flexslider" style="margin-top:30px; width:70%; margin-left:10%; height:90% !important; padding:35px">
+   <form action="mailto:votrenom@provider.com" method="post" name="contact">
+Pseudo: 
+<input name="votre nom"><p>
+Votre sexe: 
+<input type="radio" name="sexe" value="homme">un homme 
+<input type="radio" name="sexe" value="femme">une femme<p>
+<p>
+Votre message:<br>
+<textarea name="message" rows="2" cols="30"></textarea><p>
+<input type="submit" value="envoyer"></form>
+</div>
