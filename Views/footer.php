c93641fb3adf58254a152bf663be8e92e534ec13
aziza <hegeromrane@gmail.com>
2014-01-18 07:59:47 +0100

    Vx 0.2

--- Views/footer.php
+++ Views/footer.php
@@ -1,7 +1,7 @@
 </div>
 <div id="footer" style="width:230%; float:left;">
     <div style="width:20%; float:left; " align="left">Copyright © 2014 </div>
-	<div style="width:40%;" align="right"><a href="/contact">Contact</div>
+	<div style="width:40%;" align="right"><a href="./contact/index.php">Contact</div>
 </div>
 </body>
 </html>
